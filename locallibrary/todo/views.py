7757eb6e9ac31838962489b77ac02f9ca876f678
Sunil Mahato <sunilmahato@xiaomi.com>
2020-05-16 20:06:55 +0530

    added flas in models changes in html

--- locallibrary/todo/views.py
+++ locallibrary/todo/views.py
@@ -14,8 +14,9 @@ def addTodo(request):
         important =True if 'Important' in request.POST.get('checkbox2',' ') else False
         urgent = True if 'Urgent' in request.POST.get('checkbox1',' ') else False
         Datelogged = datetime.date.today()
+
         if data not in ['',None]:
-            new_item=TodoItem(content=data,Urgent=urgent,Important=important,Datelogged=Datelogged)
+            new_item=TodoItem(content=data,Urgent=urgent,Important=important,Datelogged=Datelogged,Flag=False)
             new_item.save()
         return HttpResponseRedirect('/todo/')
 
@@ -23,3 +24,10 @@ def deleteTodo(request, todo_id):
     item_to_delete=TodoItem.objects.get(id=todo_id)
     item_to_delete.delete()
     return HttpResponseRedirect('/todo/')
+
+def strikethroughTodo(request, todo_id):
+    item_to_strike=TodoItem.objects.get(id=todo_id)
+    item_to_strike.Flag=True
+    item_to_strike.save()
+    return HttpResponseRedirect('/todo/')
+
